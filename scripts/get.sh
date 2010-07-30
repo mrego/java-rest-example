18e071e658cfbdab1e5b398d90ea929e7815b6a3
Manuel Rego Casasnovas <mrego@igalia.com>
2010-07-30 14:18:48 +0200

    Added method to get just one message.

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,1 +1,1 @@
-curl -sv -X GET http://localhost:8080/rest-example/rest/messages/ | tidy -xml -i -q -utf8
+curl -sv -X GET http://localhost:8080/rest-example/rest/messages/$1 | tidy -xml -i -q -utf8
--- src/main/java/com/mastersoftwarelibre/rest/services/MessageService.java
+++ src/main/java/com/mastersoftwarelibre/rest/services/MessageService.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.mastersoftwarelibre.rest.dtos.MessageConverter;
@@ -26,6 +27,13 @@ public class MessageService {
     @Produces("application/xml")
     public MessageListDTO getMessages() {
         return MessageConverter.toDTO(messages);
+    }
+
+    @GET
+    @Produces("application/xml")
+    @Path("/{index}")
+    public MessageDTO getMessage(@PathParam("index") int index) {
+        return MessageConverter.toDTO(messages.get(index));
     }
 
     @POST
