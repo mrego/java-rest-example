d3dad500044f1917c9a808a2733afe5ac5f16905
Manuel Rego Casasnovas <mrego@igalia.com>
2010-07-30 13:37:34 +0200

    Basic POST method

---
+++ scripts/post.sh
@@ -0,0 +1,12 @@
+
+if [ $# -ne 1 ]
+then
+	echo "ERROR: Missing file" 1>&2
+	echo "Use: $0 file-name" 1>&2
+	exit 1
+else
+	file=$1
+fi
+
+curl -sv -X POST -d @$file --header "Content-type: application/xml" \
+	http://localhost:8080/rest-example/helloworld/
--- src/main/java/com/mastersoftwarelibre/rest/services/HelloWorldService.java
+++ src/main/java/com/mastersoftwarelibre/rest/services/HelloWorldService.java
@@ -1,6 +1,8 @@
 package com.mastersoftwarelibre.rest.services;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -13,6 +15,12 @@ public class HelloWorldService {
     @Produces("application/xml")
     public MessageDTO getMessage() {
         return new MessageDTO("Hello World");
+    }
+
+    @POST
+    @Consumes("application/xml")
+    public void setMessage(MessageDTO messageDTO) {
+        System.out.println("Storing message: " + messageDTO.content);
     }
 
 }
